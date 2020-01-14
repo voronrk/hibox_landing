1a2e8e7f42fbf7141e7d0a4eac5ce721282d97a1
voron <voron_76@mail.ru>
2020-01-15 00:11:20 +0300

    Add modal window for show message about success of send order. Add privecy policy but without stiles

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
           submit=document.getElementById('submit'),
           noinput1=document.getElementById('noinput1'),
           noinput2=document.getElementById('noinput2');
+          modal=document.getElementById('modal');
 
     phone.addEventListener('focus', () => {
         noinput1.style.display='none';
@@ -18,15 +19,16 @@ document.addEventListener('DOMContentLoaded', ()=> {
         noinput1.style.display='none';
         noinput2.style.display='none';
     });
+
+    modal.addEventListener('click', () => {
+        modal.style.display='none';
+    });
     
     submit.addEventListener('click', () => {
         if (phone.value=='' && email.value=='') {
             noinput1.style.display='block';
             noinput2.style.display='block'; 
         } else {
-        // console.log(phone.value);
-        // console.log(email.value);
-        
         const request = new XMLHttpRequest();
         const url = "../send.php";
         const params = "name='" + name.value + "'&phone='" + phone.value + "'&email='" + email.value + "'&text='" + text.value +"'";
@@ -36,8 +38,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
            
         request.addEventListener("readystatechange", () => {
             if(request.readyState === 4 && request.status === 200) {       
-                let savingResult=(request.responseText);
-                console.log(savingResult);
+                // let savingResult=(request.responseText);
+                // console.log(savingResult);
+                modal.style.display='flex';
             }
           });
           request.send(params);
@@ -45,31 +48,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
           phone.value="";
           email.value="";
           text.value="";
-
-            
+           
         };
 
-      
-
-        // let currentString = num.value + ';' + 
-        //                     paper.value + ';' + 
-        //                     plan.value + ';' + 
-        //                     (-1)*fact.value + ';' + 
-        //                     oper.value + ';' +
-        //                     new Date();
-        // console.log(currentString);
-        // const request = new XMLHttpRequest();
-        // const url = "../savePaper.php";
-        // const params = "paper=" + currentString;
-        // request.open("POST", url, true);
-        // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-           
-        // request.addEventListener("readystatechange", () => {
-        //     if(request.readyState === 4 && request.status === 200) {       
-        //         let savingResult=(request.responseText);
-        //     }
-        //   });
-        //   request.send(params);
     });
 
     
--- send.php
+++ send.php
@@ -9,10 +9,9 @@ $subject=mb_convert_encoding($subject, "CP1251");
 $headers = 'From: ' . $from . "\r\n" .
     'Reply-To: ' . $from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-    // mail("zayavka@printkirov.ru", $subject, $message);
     // mail("rk@printkirov.ru", $subject, $message, $headers);
-    mail("rk@printkirov.ru", $subject, $message, $headers);
-    mail("sales@hibox.pro", $subject, $message, $headers);
+    // mail("sales@hibox.pro", $subject, $message, $headers);
+    // mail("mail@kursor43.ru", $subject, $message, $headers);
     echo ('ok');
 // include "sent.php";
 ?>
